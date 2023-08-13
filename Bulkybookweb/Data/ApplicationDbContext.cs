81da97238c2434e0424d1c8a5df5e5a0f813d0eb
Tom André Granberg <tom.andre.granberg@gmail.com>
2023-08-13 11:56:43 +0200

    Add project files.

---
+++ Bulkybookweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bulkybookweb.Models;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulkybookweb.Data;
+
+public class ApplicationDbContext :DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    
+}
